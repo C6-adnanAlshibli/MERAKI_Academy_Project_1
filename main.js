af49778fe58e7d4c81487dcccd0d81c6947380ce
adnanAlshibli <adnann.shibli@gmail.com>
2022-08-12 21:38:37 +0300

    add to element for array for words

--- main.js
+++ main.js
@@ -1,17 +1,55 @@
 // at the start we must show the letters buttons
 
-// 
+//
 
-const array=[ "A", "B" ," C", " D","E" ,"F" , "G","H" , "I", "J","K" ,"L" , "M", "N", "O", "P", "Q","R","S" ,"T" , "U", "V","W" , "X", "Y", "Z"];
+const array = [
+  "A",
+  "B",
+  " C",
+  " D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
+array.forEach((e) => {
+  const button = document.createElement("button");
+  button.innerText = e;
+  const body = document.querySelector("body");
+  body.append(button);
+});
 
+const teams = ["barcelona", "madrid"];
 
-    array.forEach(e =>{
-        const button = document.createElement('button')
-        button.innerText=e;
-    const body = document.querySelector('body');
-    body.append(button);
-        
+const random = Math.floor(Math.random() * teams.length);
+console.log(random);
 
-        
-    })
+teams.forEach((element) => {
+  element.split("");
+  const p = document.createElement("p");
+  const body = document.querySelector("body");
+  body.append(p);
+//   for test
+  console.log(p);
+  // for test
+  p.innerText = "adnan";
+});
