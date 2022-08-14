6bcc3899e196395e592a3a2d7449036433507bc9
adnanAlshibli <adnann.shibli@gmail.com>
2022-08-15 01:47:23 +0300

    logic done

--- main.js
+++ main.js
@@ -3,60 +3,25 @@
 //
 const body = document.querySelector("body");
 // first div
-const Sdiv = document.querySelector('.container');
-const start = document.querySelector('#start')
-const container1 =document.querySelector('.container1')
-const container2 =document.querySelector('.container2')
-const chooseBtn = document.querySelector("#chooseBtn");
+const Sdiv = document.querySelector(".container");
+const start = document.querySelector("#start");
+const container1 = document.querySelector(".container1");
+const container2 = document.querySelector(".container2");
+const chooseBtn = document.querySelector(".chooseBtn");
 const try1 = document.querySelector(".try");
-const one = document.querySelector('.one')
-const two= document.querySelector('.two')
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 
-
-
-
-
-// Sdiv.className='startDiv'
-// const startDiv=document.querySelector('.startDiv');
-// Sdiv.innerHTML='<h1>welcome for this game</h1>'
-// body.append(Sdiv);
-// const Sbtn= document.createElement('button');
-// Sbtn.className="startButton"
-// Sbtn.innerText='START';
-// start1=document.querySelector(".startButton")
-// Sdiv.append(Sbtn)
-
-// console.log(start1,'adn',Sbtn);
-// end of first div
-// const secondDiv=document.querySelector('.secondDiv');
-
-// make another div and show it when click start button
-// second div which name is Giv 
-// const Giv = document.createElement('div');
-// Giv.className='gameDiv';
-// Giv.innerHTML='<h1>ARAB COUNTRIES</h1> <div class="secondDiv" </div> <div class="buttonsChoose"> </div>'
-// Giv.style.display='none';
-// body.append(Giv)
-// const gameDiv=document.querySelector('.gameDiv')
-// const buttonsChoose=document.querySelector('.buttonsChoose');
-// end of second div
-
-let imgArr = [ 
-  {step: './images/1.png', id:1},
-  {step: './images/2.png', id:2},
-  {step: './images/3.png', id:3},
-  {step: './images/4.png', id:4},
-  {step: './images/5.png', id:5},
-  {step: './images/6.png', id:6}
-
+let imgArr = [
+  { step: "./images/1.png", id: 1 },
+  { step: "./images/2.png", id: 2 },
+  { step: "./images/3.png", id: 3 },
+  { step: "./images/4.png", id: 4 },
+  { step: "./images/5.png", id: 5 },
+  { step: "./images/6.png", id: 6 },
 ];
-
-
-
-
-
 
 const array = [
   "A",
@@ -69,7 +34,7 @@ const array = [
   "H",
   "I",
   "J",
-  "K",          
+  "K",
   "L",
   "M",
   "N",
@@ -86,92 +51,65 @@ const array = [
   "Y",
   "Z",
 ];
-let count = 0
-let count2 =0
-array.forEach((ele,i) => {
-  const button = document.createElement("button");
-  button.innerText = ele;
-  right.append(buttonsChoose)
-  buttonsChoose.append(button);
-  button.id=ele;
-  button.addEventListener('click',(e)=>{
-    if(choose.indexOf(e.target.id) !== -1){
-      // change later 
-      button.style.backgroundColor='red';
-      const line=document.querySelectorAll('span');
-
-      // i hate < & > 
-      for(let i=0; i<choose.length; i++){
-        if(e.target.id ===choose[i]){
-          console.log(line);
-          line[i].innerText=e.target.id;
+let count = 0;
+let count2 = 0;
+array.forEach((element, index) => {
+  const buttons = document.createElement("button");
+  buttons.innerText = element;
+  Sdiv.append(container2);
+  container2.append(left);
+  left.append(right);
+  right.append(chooseBtn);
+  chooseBtn.append(buttons);
+  buttons.id = element;
+  buttons.addEventListener("click", (e) => {
+    if (choose.indexOf(e.target.id) !== -1) {
+      buttons.style.backgroundColor = "blue";
+      const span = document.querySelectorAll("span");
+      // console.log("span",span.length)
+      // span[word.indexOf(e.target.id)].innerText=(e.target.id);
+      for (let i = 0; i < choose.length; i++) {
+        if (e.target.id === choose[i]) {
+          // console.log("span[i]",span[i],span)
+          span[i].innerText = e.target.id;
           count2++;
         }
       }
-      if(count2===choose.length){
-        secondDiv.innerText='';
-        const image=document.createElement('img');
-        image.src='./images/win.png';
-        secondDiv.append(image);
-        Giv.style.display='block';
+      if (count2 === choose.length) {
+        one.innerText = "";
+        const image1 = document.createElement("img");
+        image1.src = "./images/win.png";
+        one.append(image1);
+        con3.style.display = "block";
       }
-    }else{
-      container2.innerText='';
-      const imges=document.createElement('img');
-      imges.src=imgArr[count].step;
-      secondDiv.append(imges);
+    } else {
+      one.innerText = "";
+      const image = document.createElement("img");
+      image.src = imgArr[count].step;
+      one.append(image);
       count++;
-      gameDiv.style.display='block';
     }
-    
-  })
+  });
 });
 
 const countries = ["SYRIA", "JORDAN"];
-const random = ()=>{
-  return countries[Math.floor(Math.random()* countries.length)];
-}
-const choose=random();
-//  third div
-
-// const thDiv = document.createElement('div');
-// const tryA= document.createElement('button');
-// tryA.innerText='TRY AGAIN'
-// thDiv.className='thirdDiv'
-// thDiv.append(tryA);
-// body.append(thDiv);
-// const thirdDiv=document.querySelector('thirdDiv');
-
-const play = ()=>{
-  console.log(choose,'aaa');
-  container1.style.display='none'
-    container2.style.display='block'
-choose.split('').forEach((element,index)=>{
-  if(element !== " "){
-    const lines = document.createElement('span');
-    lines.innerText=" __ "
-    lines.id=index;
-    Sdiv.append(lines);
-  }
-})
-
-}
-start.addEventListener('click',play);
-// Sbtn.addEventListener("click", () => {
-//   // console.log("Profile picture has been hovered");
-//   Sdiv.style.display='none'
-//   Giv.style.display='block'
-// });
+const random = () => {
+  return countries[Math.floor(Math.random() * countries.length)];
+};
+const choose = random();
 
 
-
-
-
-
-
-// -----------------------------------------------------------------
-// third div to reload the page to try again the game
-
-
-
-
+const play = () => {
+  console.log(choose, "aaa");
+  container1.style.display = "none";
+  container2.style.display = "block";
+  choose.split("").forEach((element, index) => {
+    if (element !== " ") {
+      const lines = document.createElement("span");
+      lines.innerText = " __ ";
+      lines.id = index;
+      Sdiv.append(lines);
+    }
+  });
+};
+start.addEventListener("click", play);
